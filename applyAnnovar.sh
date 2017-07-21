694259f212a0e97a191103409cdc9fa5c6f38236
Narendra Meena <narendrabhagri03@gmail.com>
2017-07-21 13:07:44 +0530

    delet git files
    delet unwanted files created by git mismatch repositories

--- applyAnnovar.sh
+++ applyAnnovar.sh
@@ -4,7 +4,7 @@
 #read INPUT
 #x =$(find /Users/naru/Documents/BISR/WESPipelinePaper/benchmarking  -name "*.raw.vcf")
 
-for VAR in $(find /Users/naru/Documents/BISR/WESPipelinePaper/benchmarking  -name "*.raw.vcf"); 
+for VAR in $(find $(pwd) -name "*.raw.vcf"); 
 do # Not recommended, will break on whitespace
 	perl /Users/naru/Documents/BISR/AAReviewPaper/software/annovar/table_annovar.pl "${VAR}" /Users/naru/Documents/BISR/AAReviewPaper/genome/hg19/hg19index/bwa -buildver hg19 -out "${VAR}" --otherinfo -remove -protocol ensGene -operation g -nastring . --vcfinput
 done 
