254c334f507d898a895d63f815ab11ca92b72d1b
Narendra Meena <narendrabhagri03@gmail.com>
2017-07-21 14:49:29 +0530

    copy data
    copy files with specific file extention with directory structure and empty folders

---
+++ copyingFilesWithSpecificExtention.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+#copying files from server to local 
+rsync -a --include '*/' --include '*.indel' --exclude '*' narumeena@192.168.1.17:/home/ngs/narumeena/Documents/1000GenomeSamplesBenchmarking/ /Users/naru/Documents/BISR/WESPipelinePaper/benchmarking/
