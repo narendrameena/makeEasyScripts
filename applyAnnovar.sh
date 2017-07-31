a8a957e121ed8958a56a22ba8e260181bbd7d18a
Narendra Meena <narendrabhagri03@gmail.com>
2017-07-31 11:01:51 +0530

    adding more annotation
    add snp and esp along with 1k genome for hg19 buil

--- applyAnnovar.sh
+++ applyAnnovar.sh
@@ -6,7 +6,7 @@
 
 for VAR in $(find $(pwd) -name "*.vcf"); 
 do # Not recommended, will break on whitespace
-	perl /Users/naru/Documents/BISR/software/annovar/table_annovar.pl "${VAR}" /Users/naru/Documents/BISR/genome/hg19/hg19index/bwa -buildver hg19 -out "${VAR}" --otherinfo -remove -protocol ensGene -operation g -nastring . --vcfinput
+	perl /Users/naru/Documents/BISR/software/annovar/table_annovar.pl "${VAR}" /Users/naru/Documents/BISR/genome/hg19/hg19index/bwa -buildver hg19 -out "${VAR}" --otherinfo -remove -protocol ensGene,esp6500siv2_all,avsnp147,1000g2015aug -operation g,f,f,f -nastring . --vcfinput -csvout
 done 
 
 exit 0
