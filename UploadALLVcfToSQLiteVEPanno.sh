36248bd348982d639fef483b535597a0ac93546a
Narendra Meena <narendrabhagri03@gmail.com>
2017-08-03 09:24:24 +0530

    add vep annotation
    add vep annotation for gemini

---
+++ UploadALLVcfToSQLiteVEPanno.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#author narumeena
+#loading all vcf files to sqlite db
+
+#variables
+VT=/"home/ngs/narumeena/Documents/BISR/AAnemia/BISR/software/vt/vt"
+REF="/home/ngs/narumeena/Documents/BISR/AAnemia/BISR/genome/hg19/hg19index/bwa/hg19.fasta"
+GEMINI="/home/ngs/narumeena/Documents/BISR/AAnemia/BISR/software/gemini/bin/gemini"
+VEP="/home/ngs/narumeena/Documents/BISR/AAnemia/BISR/software/ensemble-vep/vep.pl"
+for VAR in $(find $(pwd) -name "*.vcf"); 
+do # Not recommended, will break on whitespace
+    if [ -f "${VAR%.*}.vep.ann.vcf" ]  
+    then
+        rm "${VAR%.*}.vep.ann.vcf"
+    fi
+    perl "$VEP" -i "${VAR}" --cache --sift b --polyphen b --symbol --numbers --biotype --total_length -o "${VAR%.*}.vep.ann.vcf" --vcf --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE 
+    if [ -f "${VAR%.*}.vep.ann.new.vcf" ] 
+    then
+        rm "${VAR%.*}.vep.ann.new.vcf"
+    fi
+    "$VT" decompose -s "${VAR%.*}.vep.ann.vcf" | "$VT"  normalize -r "$REF" - > "${VAR%.*}.vep.ann.new.vcf"
+    if [ -f "${VAR%.*}.vep.ann.new.db" ] 
+    then
+        rm "${VAR%.*}.vep.ann.new.db"
+    fi
+    "$GEMINI" load -v "${VAR%.*}.vep.ann.new.vcf" -t snpEff "${VAR%.*}.vep.ann.new.db"
+done 
+exit 0
