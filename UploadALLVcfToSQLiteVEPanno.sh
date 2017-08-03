9cf5788d4a304c3761eebeec0b00c5c6374cb8e5
Narendra Meena <narendrabhagri03@gmail.com>
2017-08-03 10:12:35 +0530

    correct and put and opretore to find files

--- UploadALLVcfToSQLiteVEPanno.sh
+++ UploadALLVcfToSQLiteVEPanno.sh
@@ -7,9 +7,10 @@
 VT=/"home/ngs/narumeena/Documents/BISR/AAnemia/BISR/software/vt/vt"
 REF="/home/ngs/narumeena/Documents/BISR/AAnemia/BISR/genome/hg19/hg19index/bwa/hg19.fasta"
 GEMINI="/home/ngs/narumeena/Documents/BISR/AAnemia/BISR/software/gemini/bin/gemini"
-VEP="/home/ngs/narumeena/Documents/BISR/AAnemia/BISR/software/ensemble-vep/vep.pl"
-for VAR in $(find $(pwd) -name "*.vcf"); 
+VEP="/home/ngs/narumeena/Documents/BISR/AAnemia/BISR/software/ensembl-vep/vep.pl"
+for VAR in $(find $(pwd) -name "*.raw.default.vcf" && find $(pwd) -name "*.raw.vcf" && find $(pwd) -name "*.cons.vcf" && find $(pwd) -name "*.pvalue.vcf" && find $(pwd) -name "*.indel.vcf" && find $(pwd) -name "*.snp.vcf") ; 
 do # Not recommended, will break on whitespace
+    echo "$VAR"
     if [ -f "${VAR%.*}.vep.ann.vcf" ]  
     then
         rm "${VAR%.*}.vep.ann.vcf"
